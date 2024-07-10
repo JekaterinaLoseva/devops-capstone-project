a424adffd6b9bb6cbbcd8701fe9710463f59e936
JekaterinaLoseva <loseva.jekaterina@gmail.com>
2024-07-10 22:10:02 +0300

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -60,6 +60,7 @@ def create_accounts():
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -75,6 +76,7 @@ def list_accounts():
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
+
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
     """
@@ -87,7 +89,6 @@ def get_accounts(account_id):
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     return account.serialize(), status.HTTP_200_OK
     
-
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
@@ -118,8 +119,7 @@ def delete_accounts(account_id):
     app.logger.info("Request to delete an Account with id: %s", account_id)
     account = Account.find(account_id)
     if account:
-        account.delete()
-        
+        account.delete()        
     return "", status.HTTP_204_NO_CONTENT
 
 ######################################################################
