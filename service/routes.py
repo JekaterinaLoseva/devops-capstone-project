c846b3ad39d54d98094ec82228fbbeaa10e6b299
JekaterinaLoseva <loseva.jekaterina@gmail.com>
2024-07-10 22:18:27 +0300

    fix

--- service/routes.py
+++ service/routes.py
@@ -94,6 +94,8 @@ def get_accounts(account_id):
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
+
+
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """
@@ -121,7 +123,7 @@ def delete_accounts(account_id):
     app.logger.info("Request to delete an Account with id: %s", account_id)
     account = Account.find(account_id)
     if account:
-        account.delete()        
+        account.delete()
     return "", status.HTTP_204_NO_CONTENT
 
 ######################################################################
